11b9abf8bad923d5a56dc7a09aa100350885dacb
TeJiang <77681730+TeJiang@users.noreply.github.com>
2024-04-16 07:47:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QAction
+from PyQt6.QtWidgets import (
+    QApplication, QComboBox, QDateEdit, QDateTimeEdit, QDial,
+    QDoubleSpinBox, QFontComboBox, QLabel, QLCDNumber, QLineEdit,
+    QMainWindow, QProgressBar, QPushButton, QRadioButton,
+    QSlider, QSpinBox, QTimeEdit, QVBoxLayout, QWidget, QCheckBox
+)
 
 
 class MainWindow(QMainWindow):
@@ -7,13 +14,15 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle('My App')
-        button = QPushButton('Click Me', self)
-
-        s
+        layout = QVBoxLayout()
+        widget = QComboBox()
+        widget.addItems(['A', 'B', 'C', 'D'])
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
 
 
 app = QApplication(sys.argv)
-window = QMainWindow()
+window = MainWindow()
 window.show()
 
 app.exec()
