5649bcde39ffb2d245ac10d82f35ecfff78d77bc
TeJiang <77681730+TeJiang@users.noreply.github.com>
2024-04-17 00:31:22 +0200

    add window.ui

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import random
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
-
 import sys
+from window import Ui_MainWindow
 
 
 class AnotherWindow(QWidget):
@@ -11,22 +12,16 @@ class AnotherWindow(QWidget):
     def __init__(self):
         super().__init__()
         layout = QVBoxLayout()
-        self.label = QLabel("Another Window")
+        self.label = QLabel("Another Window % d"%random.randint(0, 100))
         layout.addWidget(self.label)
         self.setLayout(layout)
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
 
-    def __init__(self):
-        super().__init__()
-        self.button = QPushButton("Push for Window")
-        self.button.clicked.connect(self.show_new_window)
-        self.setCentralWidget(self.button)
-
-    def show_new_window(self, checked):
-        self.w = AnotherWindow()
-        self.w.show()
+    def __init__(self, *args, obj=None, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
 
 
 app = QApplication(sys.argv)
