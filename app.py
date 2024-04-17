54b4e37d51e909bd6c6e8a9fbcc96ac7d1fd7223
Te Jiang <te.jiang@universite-paris-saclay.fr>
2024-04-17 11:17:03 +0200

    Delete files not useful

--- app.py
+++ app.py
@@ -1,30 +1,25 @@
-import random
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
+from PyQt6 import QtWidgets, uic
+from pyqtgraph import PlotWidget
+import pyqtgraph as pg
 import sys
-from window import Ui_MainWindow
 
-
-class AnotherWindow(QWidget):
-    """
-    This "window" is a QWidget. If it has no parent, it
-    will appear as a free-floating window as we want.
-    """
-    def __init__(self):
-        super().__init__()
-        layout = QVBoxLayout()
-        self.label = QLabel("Another Window % d"%random.randint(0, 100))
-        layout.addWidget(self.label)
-        self.setLayout(layout)
-
-
-class MainWindow(QMainWindow, Ui_MainWindow):
-
-    def __init__(self, *args, obj=None, **kwargs):
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-        self.setupUi(self)
 
+        uic.loadUi('MainWindow.ui', self)
+        self.plot(
+            [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+            [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20][::-1],
+        )
+    def plot(self, h, t):
+        self.graphwidget.plot(h, t)
 
-app = QApplication(sys.argv)
-w = MainWindow()
-w.show()
-app.exec()
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec())
+
+if __name__ == '__main__':
+    main()
